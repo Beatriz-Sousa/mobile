1fe479ddd8eeec530f165bd1cc44765bdf702dc7
Beatriz Sousa <145013810+Beatriz-Sousa@users.noreply.github.com>
2024-03-26 16:24:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -22,3 +22,38 @@ window.addEventListener("scroll", function () {
         document.querySelector("#navbar").style.opacity = 1;
     }
 });
+
+class MobileNavbar {
+    constructor(mobileMenu, navbarItems, navbarLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navbarItems = document.querySelector(navbarItems); // Modificado
+        this.navbarLinks = document.querySelectorAll(navbarLinks);
+        this.activeClass = "active";
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navbarLinks.forEach((link, index) => { // Modificado
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+
+    handleClick() {
+        this.navbarItems.classList.toggle(this.activeClass); // Modificado
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
