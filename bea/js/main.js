a13bb0b62e95d105a3781ec9d7d82432edede282
Beatriz Sousa <145013810+Beatriz-Sousa@users.noreply.github.com>
2024-03-27 17:08:06 -0300

    Add files via upload

---
+++ bea/js/main.js
@@ -0,0 +1,78 @@
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
+
+// Sticky menu background
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 150) {
+        document.querySelector("#navbar").style.opacity = 0.9;
+    } else {
+        document.querySelector("#navbar").style.opacity = 1;
+    }
+});
+
+/* class MobileNavbar {
+    constructor(mobileMenu, navbarItems, navbarLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navbarItems = document.querySelector(navbarItems); // Modificado
+        this.navbarLinks = document.querySelectorAll(navbarLinks);
+        this.activeClass = "active";
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navbarLinks.forEach((link, index) => { // Modificado
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+
+    handleClick() {
+        this.navbarItems.classList.toggle(this.activeClass); // Modificado
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+} */
+
+
+
+const menuOpener = document.querySelector('.mobile-menu')
+const menuCloser = document.querySelector('.menu-closer')
+const teste =  document.querySelector('#navbar-items')
+
+const openMenu = () => {
+    teste.style.transform = 'translateX(0)'
+    menuOpener.style.display = 'none'
+}
+
+const closeMenu = () => {
+    teste.style.transform = 'translateX(100%)'
+    menuOpener.style.display = 'block'
+}
+
+menuOpener.addEventListener('click', openMenu)
+menuCloser.addEventListener('click', closeMenu)
